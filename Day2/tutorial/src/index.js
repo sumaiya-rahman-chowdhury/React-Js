067101599cec25e5f934391ea71673210313daa5
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-02 12:25:47 -0800

    update

---
+++ Day2/tutorial/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Greeting() {
+  return <h1>
+    This is Sumaiya,This is my first Component
+
+  </h1>;
+}
+ReactDOM.render(<Greeting />, document.getElementById('root'));
