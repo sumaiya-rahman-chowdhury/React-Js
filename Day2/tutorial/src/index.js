f5b596f6360c24914152569d294177c0e158df69
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-03 02:02:14 -0800

    arrow func

--- Day2/tutorial/src/index.js
+++ Day2/tutorial/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-function Greeting() {
-  return <h1>
-    This is Sumaiya,This is my first Component
+//function Greeting() {
+  //return <h1>
+    //This is Sumaiya,This is my first Component
 
-  </h1>;
+ // </h1>;
+//}
+const Greeting =()=>{
+  return React.createElement('h1',{},'This is Sumaiya');
 }
 ReactDOM.render(<Greeting />, document.getElementById('root'));
