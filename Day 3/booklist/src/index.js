d240003ae4d9f1ba3b3f78768be035d44fe4807e
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-04 00:03:02 -0800

    update

--- Day 3/booklist/src/index.js
+++ Day 3/booklist/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -15,19 +16,14 @@ function Booklist() {
 
   );
 }
+const arthor = 'Prince Harry The Duke of Sussex';
+const bookname = 'Spare';
 const Book = () => {
   return <article>
-    <Image />
-    <Title />
-    <Arthor />
+    <img src='https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg'/>
+    <h1 style={{letterSpacing:'0.7rem'}}>{bookname}</h1>
+    <h4>{arthor}</h4>
   </article>
 }
-const Image = () => {
-  return (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <img src='https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg' />
-  );
-}
-const Title = () => <h1>Spare</h1>
-const Arthor = () => <h4> Prince Harry The Duke of Sussex</h4>
+
 ReactDOM.render(<Booklist></Booklist>, document.getElementById('root'));
