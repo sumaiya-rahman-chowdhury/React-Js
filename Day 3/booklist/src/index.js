94151bf2f16933a4072a0616c2d369aefa1d8406
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-06 10:03:04 -0800

    update

--- Day 3/booklist/src/index.js
+++ Day 3/booklist/src/index.js
@@ -2,11 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg',
+  arthor: 'Prince Harry The Duke of Sussex', bookname: 'Spare', price: '$9.99'
+}
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/817-Vrzp+tL._AC_UL254_SR254,254_.jpg',
+  arthor: 'Alice Schertle', bookname: "Little Blue Truck's Valentine ",price:'8.99'
+}
+
 function Booklist() {
   return (
     <section className='list'>
-      <Book />
-      <Book />
+      <Book img={firstBook.img} bookname={firstBook.bookname} arthor={firstBook.arthor} price={firstBook.price} />
+      <Book img={secondBook.img} bookname={secondBook.bookname}arthor={secondBook.arthor} price={secondBook.price}/>
       <Book />
       <Book />
       <Book />
@@ -16,14 +25,16 @@ function Booklist() {
 
   );
 }
-const arthor = 'Prince Harry The Duke of Sussex';
-const bookname = 'Spare';
-const Book = () => {
-  return <article>
-    <img src='https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg'/>
-    <h1 style={{letterSpacing:'0.7rem'}}>{bookname}</h1>
-    <h4>{arthor}</h4>
+
+const Book = (props) => {
+  return (<article>
+    <img src={props.img} />
+    <h1>{props.bookname}</h1>
+    <h4>{props.arthor}</h4>
+    <p>{props.price}</p>
+
   </article>
+  );
 }
 
 ReactDOM.render(<Booklist></Booklist>, document.getElementById('root'));
