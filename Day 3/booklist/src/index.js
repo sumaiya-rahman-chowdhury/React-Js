92235b67fb21df79ca2bb08f9e57ce2ad9a9d076
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-07 09:58:15 -0800

    updt

--- Day 3/booklist/src/index.js
+++ Day 3/booklist/src/index.js
@@ -4,12 +4,79 @@ import ReactDOM from 'react-dom';
 import './index.css';
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg',
-    arthor: 'Prince Harry The Duke of Sussex', bookname: 'Spare', price: '$9.99'
+    arthor: 'Prince Harry The Duke of Sussex',
+    bookname: 'Spare',
+    price: '$9.99',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/817-Vrzp+tL._AC_UL254_SR254,254_.jpg',
-    arthor: 'Alice Schertle', bookname: "Little Blue Truck's Valentine ", price: '8.99'
+    arthor: 'Alice Schertle', bookname: "Little Blue Truck's Valentine ", price: '8.99',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 4,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 5,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 6,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 7,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 8,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL254_SR254,254_.jpg',
+    bookname: 'It Ends with Us: A Novel (1)',
+    arthor: 'Colleen Hoover',
+    price: '$9.99'
+  },
+  {
+    id: 9,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg',
+    arthor: 'Prince Harry The Duke of Sussex',
+    bookname: 'Spare',
+    price: '$9.99',
+  },
+  {
+    id: 10,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg',
+    arthor: 'Prince Harry The Duke of Sussex',
+    bookname: 'Spare',
+    price: '$9.99',
+  },
+  {
+    id: 11,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL127_SR127,127_.jpg',
+    arthor: 'Prince Harry The Duke of Sussex',
+    bookname: 'Spare',
+    price: '$9.99',
   }
 ];
 
@@ -20,9 +87,9 @@ function Booklist() {
     <section className='list'>
       {
         books.map((book) => {
-          const { img, arthor, bookname, price } = book;
-          return(
-            <Book book = {book}></Book>
+          //const { img, arthor, bookname, price } = book;
+          return (
+            <Book key={book.id} book={book}></Book>
           );
         })
       }
