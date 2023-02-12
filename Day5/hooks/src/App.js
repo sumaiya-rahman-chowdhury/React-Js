054ece93d4a10aa5392800bd278e8d7f7ecd6f1a
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-12 06:43:32 -0800

    react hook-useState

---
+++ Day5/hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example';
+import Final from './tutorial/1-useState/final/1-error-example';
+function App() {
+    return(
+      <div className='container'>
+        <Setup></Setup>
+       
+      </div>
+    );
+  
+}
+
+export default App
