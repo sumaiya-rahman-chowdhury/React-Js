8c0f0417cda4c799ebdfade44a02c46e1b547fa2
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-12 12:21:21 -0800

    useState

--- Day5/hooks/src/App.js
+++ Day5/hooks/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/1-error-example';
-import Final from './tutorial/1-useState/final/1-error-example';
+import Setup from './tutorial/1-useState/setup/2-useState-basics';
 function App() {
     return(
       <div className='container'>
--- Day5/hooks/src/tutorial/1-useState/setup/2-useState-basics.js
+++ Day5/hooks/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const [text, setText] = useState('Random Title');
+  const handleClick = () =>{
+if(text === 'Random Title'){
+  setText('Hello World')
+}
+else{
+  setText('Random Title')
+}
+  }
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Click Here</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
