12ccf8f1f9e8dc8570810ae61bba5e2501ceaa5b
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-13 15:04:53 -0800

    counter app

--- Day5/hooks/src/tutorial/1-useState/setup/4-useState-object.js
+++ Day5/hooks/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name:'Sumaiya',
+    age:24,
+    message:'Rihanna Superbowl'
+  });
+  const NewMessage = () =>{
+    setPerson({...person,message:'Jay Z Best rapper'})
+  }
+  
+  return (
+    <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button className='btn' onClick={NewMessage}>New Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- Day5/hooks/src/tutorial/1-useState/setup/5-useState-counter.js
+++ Day5/hooks/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset =()=>{
+setValue(0);
+  }
+  return (
+    <><section style={{ margin: '4rem 0', backgroundColor: 'lightblue'}}>
+      <h2>{value}</h2>
+      <button className='btn' onClick={() => setValue(value
+        - 1)} >decrement</button>
+      <button className='btn' onClick={reset}>reset</button>
+      <button className='btn' onClick={() => setValue(value + 1
+      )}>increment</button>
+    </section>
+
+    </>
+  );
 };
 
 export default UseStateCounter;
