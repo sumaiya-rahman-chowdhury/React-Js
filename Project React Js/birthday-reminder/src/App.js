3d665e66b55ebabf1b18f715e30a34a766202682
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-14 12:33:14 -0800

    modified/added single item deletation

--- Project React Js/birthday-reminder/src/App.js
+++ Project React Js/birthday-reminder/src/App.js
@@ -3,12 +3,17 @@ import data from './data';
 import List from './List';
 function App() {
   const [people,setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id !== id);
+    });
+  }
   return (
     <main>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
-        <List people={people}></List>
-        <button onClick={()=>{setPeople([])}}>clear all</button>
+        <List people={people} removePerson={removePerson}></List>
+        <button className='btn1' onClick={()=>{setPeople([])}}>clear all</button>
       </section>
     </main>
   );
--- Project React Js/birthday-reminder/src/List.js
+++ Project React Js/birthday-reminder/src/List.js
@@ -1,26 +1,30 @@
 import React from 'react';
 
-const List = ({ people }) => {
+const List = ({ people,removePerson}) => {
+
+
   return (
     <>
-    {
-      people.map((person) => {
-        const { id, age, name, image } = person;
-        return (
-          <article key ={id} className='person'>
-            <img src={image} alt={name}></img>
-            <div>
-              <h4>{name}</h4>
-              <p>{age}</p>
-            </div>
-          </article>
-        )
+    
+      {
+        people.map((person) => {
+          const { id, age, name, image } = person;
+          return (
+            <article key={id} className='person'>
+              <img src={image} alt={name}></img>
+              <div>
+                <h4>{name}</h4>
+                <p>{age}</p>
+                <button className='btn2' onClick={()=>removePerson(id)} >remove</button>
+              </div>
+            </article>
+          )
 
-      })
-    }
+        })
+      }
     </>
   );
-  
+
 };
 
 export default List;
