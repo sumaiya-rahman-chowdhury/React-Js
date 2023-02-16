cfafd0d106a564b500f8cb4e1c73113b797a3495
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-02-16 10:15:18 -0800

    fetch

--- Day5/hooks/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ Day5/hooks/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,52 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isloading, setisLoadin] = useState(true);
+  const [isError, setError] = useState(false);
+  const [user, setUser] = useState('default user')
+
+  /* const getUser = async() => {
+
+    const  response= await fetch(url);
+    const users = await response.json();
+    setUser(users);
+
+  }
+  useEffect(()=>{
+    getUser();
+  },[]); */
+
+  useEffect(() => {
+    setisLoadin(true);
+    fetch(url)
+    .then((resp) =>{
+      resp.json()
+    })
+    .then((user)=> {
+      const {login} = user;
+      setUser(login)
+      setisLoadin(false)
+    })
+    .catch((error)=>console.log(error));
+  },[]);
+
+  if (isloading) {
+    return (
+      <>
+        <h2>Loading....</h2>
+      </>
+    );
+  }
+  if (isError) {
+    return (
+      <h1>Error....</h1>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
